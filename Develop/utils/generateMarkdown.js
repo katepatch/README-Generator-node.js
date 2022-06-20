20fb3b21a52f2ccb469cfa9006b697028c9e194e
kate patch <kate.epatch@gmail.com>
2022-06-20 16:32:59 -0600

    left note about why I want to combine the functions into one

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -9,7 +9,7 @@ function renderLicenseBadge(license) {
   return badges[license]
 }
 
-//
+//I want to try to combine the three functions into one.
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 // function renderLicenseLink(license) {
