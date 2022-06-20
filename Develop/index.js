9de674dcf0baab6977e2efedba5fd6d3323c8575
kate patch <kate.epatch@gmail.com>
2022-06-20 13:06:58 -0600

    added more questions

--- Develop/index.js
+++ Develop/index.js
@@ -12,7 +12,17 @@ const questions = [
         type: 'input',
         name: 'description',
         message: 'Please give a brief description of your new project.',
-    }
+    }, 
+    {
+        type: 'input',
+        name: 'table of contents',
+        message: 'Please list the Table of Contents for each section of README',
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'Please describe the installation instructions',
+    },
 ];
 
 // TODO: Create a function to write README file
